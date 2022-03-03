efd4f686049edfbba790630a261423293b105aaa
Lucas Gabriel Devigili <lucasdevigili@hotmail.com>
2022-03-02 23:15:53 -0300

    Aula 1

---
+++ Aula 1 - Tipos Primitívos/type-number.js
@@ -0,0 +1,24 @@
+// tipo Number
+
+const meuNumero =3;
+
+const primeiroNumero =1;
+const segundoNumero =2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(`Número Inteiro: ` + operacaoMatematica)
+
+//ponto flutuante (Números decimáis)
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperação = primeiroNumero / numeroPontoFlutuante;
+
+console.log(`Número Decimal: ` + novaOperação)
+
+// NaN -> Not A Number (não é um número)
+
+const alura = "Alura";
+console.log(`NaN: ` + alura * primeiroNumero)
---
+++ Aula 1 - Tipos Primitívos/type-string.js
@@ -0,0 +1,17 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = 'O Leo disse "oi!"';
+console.log(citacao)
+
+//concatenação (+)
+
+const citacaoDois = "Meu nome é ";
+const meuNome = "Lucas";
+
+console.log(citacaoDois + meuNome)
+
+
+//template string OU template lieral
