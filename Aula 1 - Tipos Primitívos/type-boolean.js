c92eb27c34b2eca03a995ada9d3807d757ecb45a
Lucas Gabriel Devigili <lucasdevigili@hotmail.com>
2022-04-19 19:38:04 -0300

    Alura introdução ao VUE

---
+++ Aula 1 - Tipos Primitívos/type-boolean.js
@@ -0,0 +1,18 @@
+// (false)
+
+// const primeiroNumero = 5;
+// const segundoNumero = 10;
+
+// console.log(primeiroNumero === segundoNumero);
+
+// (true)
+
+// const primeiroNumero = 5;
+// const segundoNumero = 5;
+
+// console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2)
